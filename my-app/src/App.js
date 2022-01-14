637535b0586393612336d2144458744a2121c309
BuildTools <unconfigured@null.spigotmc.org>
2022-01-13 23:57:40 -0300

    Primeiro componente criado

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Tasks from './components/Tasks';
+import './App.css';
 
 const App = () => {
-  const message = 'Hellow World!'
-  return <h1>{message}</h1>;
+    const [tasks, setTasks] = useState([
+      {
+        id: '1',
+        title: 'Estudar Programação',
+        completed: false,
+      },
+      {
+        id: '2',
+        title: 'Ler Livros',
+        completed: true,
+      },
+    ]);
+
+    return (
+        <>
+            <div className='container'>
+                <Tasks tasks={tasks}/> 
+            </div>;
+        </>
+    );
 };
 
 export default App;
---
+++ my-app/src/components/Tasks.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tasks = ({tasks}) => {
+    return <h1>tasks</h1>
+};
+
+export default Tasks;
