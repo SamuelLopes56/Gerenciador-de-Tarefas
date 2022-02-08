7f94ea1cccd157517f9d7e3e7c22f90c97a0e20f
BuildTools <unconfigured@null.spigotmc.org>
2022-02-08 00:33:22 -0300

    Agora é possível destacar as tarefas feitas clicando nelas.

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 import './App.css';
+import Task from './components/Task';
 
 const App = () => {
     const [tasks, setTasks] = useState([
@@ -19,6 +20,16 @@ const App = () => {
       },
     ]);
 
+    const handleTaskClick = (taskId) => {
+        const newTasks = tasks.map((task) => {
+            if (task.id == taskId) return {... task, completed: !task.completed}
+
+            return task;
+        })
+        
+        setTasks(newTasks);
+    }
+
     const handleTaskAddition = (taskTitle) => {
       const newTasks = [... tasks, {
         title: taskTitle,
@@ -33,7 +44,7 @@ const App = () => {
         <>
             <div className='container'>
                 <AddTask handleTaskAddition={handleTaskAddition} />
-                <Tasks tasks={tasks} /> 
+                <Tasks tasks={tasks} handleTaskClick={handleTaskClick} /> 
             </div>;
         </>
     );
